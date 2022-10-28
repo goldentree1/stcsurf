10754e3c991c4938416cd48ccee0531c4c4d712d
eb <elliott.b1097@gmail.com>
2022-10-29 08:42:09 +1300

    Update connectMongo.js

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 const MONGO_URL = process.env.MONGO_ATLAS_URL;
 
-export function connectMongo(){
+export async function connectMongo(){
     console.log("CONNECTED")
     return mongoose.connect(MONGO_URL);
 }
-export function disconnectMongo(){
+export async function disconnectMongo(){
     console.log("DISCONNECTED")
     mongoose.disconnect()
 }
