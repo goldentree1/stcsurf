8324f71dcc47c0a1bda10a51f8adb45517cd56fd
eb <elliott.b1097@gmail.com>
2022-10-29 17:30:06 +1300

    Update update.js

--- server/update.js
+++ server/update.js
@@ -1,19 +1,23 @@
 import { schedule } from '@netlify/functions';
 import { Forecast } from 'models/Forecast';
+import { connectMongo } from 'utils/connectMongo';
 import { getMetOceanDataByLocation } from 'utils/metOcean';
 import { getAllLocationsData } from 'utils/location';
 
 const updateForecasts = async function (event, context) {
+    await connectMongo();
     const locations = await getAllLocationsData();
     locations.forEach(async (location) => {
         const data = await getMetOceanDataByLocation(location);
+        console.log("DATA RETRIEVED: " + data);
         const forecast = new Forecast({
             data,
             location,
             retrieved: new Date(),
             website: "metocean"
         });
-        await forecast.save()
+        const saved = await forecast.save()
+        console.log("SAVED FORECAST?: " + saved);
     });
     //TODO: trigger a NextJS re-build here.
     return {
