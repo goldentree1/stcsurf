e662f96b91e31d71f3bdb002310be55a2e04fbc3
eb <elliott.b1097@gmail.com>
2022-11-17 16:56:12 +1300

    fix err 2

--- components/forecast/SwellChart.js
+++ components/forecast/SwellChart.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './ChartJS/index.js';
+import './chartjs/index.js';
 import { Line } from 'react-chartjs-2';
-import constructDirectionArrowAnnotations from './ChartJS/annotateDirArrows.js';
+import constructDirectionArrowAnnotations from './chartjs/annotateDirArrows.js';
 
 export default function SwellChart({ data }) {
     return (
--- components/forecast/WindChart.js
+++ components/forecast/WindChart.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import './ChartJS/index.js';
+import './chartjs/index.js';
 import { Line } from 'react-chartjs-2';
-import constructDirectionArrowAnnotations from './ChartJS/annotateDirArrows';
+import constructDirectionArrowAnnotations from './chartjs/annotateDirArrows';
 
 export default function WindChart({ data }) {
     return (
