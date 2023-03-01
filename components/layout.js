367671ffa10991c5767301861b4b3dba3837ceb3
eb <elliott.b1097@gmail.com>
2023-03-02 11:58:34 +1300

    Revert "add page title"
    This reverts commit 44baa7c0acc2ce973567aa23d0065f49bc0560d5.

--- components/layout.js
+++ components/layout.js
@@ -7,9 +7,6 @@ export default function Layout({ children, page }) {
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content="Surf Forecast" />
-                <title>
-                    St-Clair Forecast ~ by EB
-                </title>
             </Head>
             <main>
                 {children}
